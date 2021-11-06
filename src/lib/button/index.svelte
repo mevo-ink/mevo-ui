193001f4f67821f65b77eafaae5dc6be2fe2cc1c
Arun Mathiyalakan <friday99@outlook.it>
2021-11-06 18:02:43 +0100

    Add sample button component

---
+++ src/lib/button/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let size = "md"
+</script>
+
+<button class={size}>
+  <slot />
+</button>
+
+<style>
+  button {
+    margin: 0;
+  }
+
+  .sm {
+    width: 100px;
+    height: 50px;
+  }
+
+  .md {
+    width: 200px;
+    height: 100px;
+  }
+</style>
